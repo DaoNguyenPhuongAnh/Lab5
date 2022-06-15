d19f5c30e8589ef770d0ec2b4e0d9baab8279e3b
PHUONG ANH <phuonganh@MacBook-Pro-cua-PHUONG.local>
2022-06-15 19:37:20 +0700

    Lab5

---
+++ src/components/DishDetailComponent.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import { Card, CardBody, CardImg, CardTitle, CardText } from "reactstrap";
+
+
+class DishDetail extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            SelectComments: null
+        };
+    }
+    onDishComments(comments) {
+        this.setState({ SelectComments: comments })
+
+    }
+    renderComments(comments) {
+        return (
+            <div className="ml-5">
+                <h1 className=" text-primary">Comments</h1>
+                {comments.map((commentIndex) => {
+                    return (
+                        <div key={commentIndex.id}>
+
+
+                            <div>
+                                <p>{commentIndex.comment}</p>
+                                <p>-- {commentIndex.author},
+                                    &nbsp;
+                                    {commentIndex.date}
+                                </p>
+                            </div>
+
+                        </div>
+                    )
+                }
+                )}
+            </div>
+        );
+    }
+    renderDish(dish) {
+        // this.onDishComment(dish.comments);
+
+        return (
+            <div key={dish.id} className="col-md-5 mt-1 ">
+                <Card>
+                    <CardImg
+                        width="100%"
+                        src={dish.image}
+                        className="flex-grow-2 img-responsive"
+                    />
+                    <CardBody>
+                        <CardTitle className="col-md-12">
+
+                            <h3>{dish.name}</h3>
+                        </CardTitle>
+                        <CardText className="col-md-12">
+
+                            {dish.description}
+                        </CardText>
+                    </CardBody>
+                </Card>
+
+
+
+
+
+            </div>
+        )
+
+
+    }
+
+
+    render() {
+
+        if (this.props.selectDish != null) {
+            return (
+                <div className="row">
+                    {this.renderDish(this.props.selectDish)}
+                    {this.renderComments(this.props.selectDish.comments)}
+                </div>
+            )
+        }
+        else {
+            return (
+                <div></div>
+            )
+        }
+
+
+
+    }
+}
+
+export default DishDetail;
